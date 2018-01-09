b4d8d6a81165385087ffa9025b96f5c96a7a1393
Rolf Plagmeijer <sausage948@gmail.com>
2018-01-09 15:07:24 +0100

    Removed some trivial equivalence classes.

--- Main.py
+++ Main.py
@@ -50,6 +50,13 @@ def displayJoinMatrix(joinMatrix):
     print("")
 
 
+def isDiagonalIndex(jmIndex):
+    if jmIndex == 0 or jmIndex == 2 or jmIndex == 5:
+        return True
+    else:
+        return False
+
+
 def switchOutputValuesInJoinMatrix(joinMatrix, permutation):
     newJoinMatrix = 0
     permutation.extend([3])
@@ -129,9 +136,54 @@ for permutationIndex in range(1, 6):
     newEquivalenceClasses = []
     toSkip = []
 
+# for eqC in equivalenceClasses:
+#     representative = next(iter(eqC))
+#     displayJoinMatrix(representative)
+
+# print("Number of matrices: " + str(len(equivalenceClasses)))
+
+print("======================================================================")
+
+# Remove equivalence classes that contain 3 or fewer non-empty values
+
+
+def numberOfNonEmptyStates(index, state):
+    if state == 3:
+        return 0
+    else:
+        return 1 if isDiagonalIndex(index) else 2
+
+
+nonTrivialEquivalenceClasses = []
 for eqC in equivalenceClasses:
+    representative = next(iter(eqC))
+    digits = BitOperations.getAllDigits(representative)
+    numberNEStates = sum(list(map(lambda t: numberOfNonEmptyStates(t[0], t[1]), enumerate(digits))))
+    if numberNEStates > 3:
+        nonTrivialEquivalenceClasses.append(eqC)
+
+# Remove equivalence classes that have an isolated entry
+newNonTrivialEquivalenceClasses = []
+for eqC in nonTrivialEquivalenceClasses:
+    representative = next(iter(eqC))
+    digits = BitOperations.getAllDigits(representative)
+    b = digits[1] == 3
+    d = digits[3] == 3
+    e = digits[4] == 3
+    if not ((b and d) or (b and e) or (d and e)):
+        newNonTrivialEquivalenceClasses.append(eqC)
+
+nonTrivialEquivalenceClasses = newNonTrivialEquivalenceClasses
+newNonTrivialEquivalenceClasses = []
+
+# Output
+
+for eqC in nonTrivialEquivalenceClasses:
     representative = next(iter(eqC))
     displayJoinMatrix(representative)
 
-# ---Output--- #
-print("Number of matrices: " + str(len(equivalenceClasses)))
+print("Number of matrices: " + str(len(nonTrivialEquivalenceClasses)))
+
+
+
+
