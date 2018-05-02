07f37d4c7db2de7314e5a3d808f9da0cf0491073
Rolf Plagmeijer <sausage948@gmail.com>
2018-05-02 16:31:17 +0200

    Added code that tests filterability.

--- MatrixTest.py
+++ MatrixTest.py
@@ -1,6 +1,5 @@
-import numpy.linalg
-import copy
-import math
+from scipy.optimize import linprog
+
 
 startingMatrix = [
                         [1, 0, 0, 0, 0, 0],
@@ -11,50 +10,11 @@ startingMatrix = [
                         [0, 0, 0, 1, 1, 2]
                  ]
 
+c = [1, 1, 1, 1]
+Aub = [[0, -1, 0, 0], [0, 0, 0, -1], [0, 2, -1, -2], [-1, -1, -1, -1]]
+bub = [-0.001, -0.001, -0.001, -0.001]
+Aeq = [[-1, 0, 0, 0], [0, 0, -1, 0], [0, 1, -1, -1]]
+beq = [0, 0, 0]
+res = linprog(c, A_ub=Aub, b_ub=bub, A_eq=Aeq, b_eq=beq, options={"disp": True})
 
-def rankAfterFilter(matrix, columnIndices):
-    copyOfMatrix = copy.deepcopy(matrix)
-    for row in copyOfMatrix:
-        for index in range(0, 6):
-            if index in columnIndices:
-                row[index] = 0
-    rank = numpy.linalg.matrix_rank(copyOfMatrix)
-    return rank
-
-
-def filterableSetsIntro(matrix):
-    boolList = [False] * 64
-    filterableSets(matrix, [0, 1, 2, 3, 4, 5], 0, boolList, [-1] * 64)
-    return boolList
-
-
-def filterableSets(matrix, filterSet, score, boolList, rankList):
-    rankList[score] = rankAfterFilter(matrix, filterSet)
-    filterabilityDecided = False
-    for join in filterSet:
-        newFilterSet = copy.copy(filterSet).remove(join)
-        newScore = score + math.pow(2, join)
-        if rankList[newScore] == -1:
-            rankList[newScore] = rankAfterFilter(matrix, newFilterSet)
-            filterableSets(matrix, newFilterSet, newScore, boolList, rankList)
-        if rankList[newScore] == rankList[score]:
-            filterabilityDecided = True
-            boolList[score] = False
-        elif rankList[newScore] == rankList[score] + 1:
-            continue
-        else:
-            print("An error has occurred in filterableSets")
-    if not filterabilityDecided:
-        boolList[score] = True
-
-
-testMatrix = ([[1, 0, 0],
-               [0, 1, 1],
-               [2, 1, 0],
-               [0, 1, 2]])
-
-boolllll = [False] * 8
-filterableSets(testMatrix, [0, 1, 2], 0, boolllll, [-1] * 8)
-print(boolllll)
-
-x = 3
+print(res)
